21137b2aae61d5d3321bfbe5920d074d49328d45
kimuhooo <fantianmucun@gmail.com>
2024-09-30 03:50:31 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Headerコンポーネントに対応するCSSファイルをインポート
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <Link to="/" className="logo">
+        Venus
+      </Link>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/locations">Locations</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
